e9a2d462513c0b0098f1827c648434647ee97d89
Alexander Shavrov <alexander.shavrov@mail.ru>
2023-10-17 02:11:44 +0300

    Update build configuration

---
+++ multithreading/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("java")
+
+    // Apply the application plugin to add support for building a CLI application in Java.
+    application
+}
+
+group = "com.wingmann"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
+
+dependencies {
+    // This dependency is used by the application.
+    implementation("com.google.guava:guava:32.1.1-jre")
+}
+
+// Apply a specific Java toolchain to ease working on different environments.
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("com.wingmann.useful.multithreading.Main")
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = application.mainClass
+    val dependencies = configurations
+            .runtimeClasspath
+            .get()
+            .map { zipTree(it) }
+
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+}
